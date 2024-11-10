d671171b816306b4aa8d0a097087a8cc9b7e460c
likeauser9 <shinrinyoku0001@gmail.com>
2024-11-10 13:28:31 +0000

    типа первый коммит

---
+++ src/sections/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+
+export (
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer
+)
